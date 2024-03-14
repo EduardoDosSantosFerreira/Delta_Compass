eabc8e8e2a2678a39716deed52499bb94e08f16e
EduardoDosSantosFerreira <eduardosferreira69@gmail.com>
2024-03-13 21:48:44 -0300

    update_v3

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -4,5 +4,8 @@ const body = document.body;
 darkModeSwitch.addEventListener('change', () => {
     body.classList.toggle('dark-mode');
 });
+function toggleDarkMode() {
+    var darkModeSwitch = document.getElementById('darkModeSwitch');
+    document.body.classList.toggle('dark-mode', darkModeSwitch.checked);
+  }
 // Dark Mode End
-
